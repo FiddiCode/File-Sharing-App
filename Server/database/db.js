ff955c5d3ada44337ce9cb75a7932577dd079539
fiddicode <coderfardeen@gmail.com>
2023-07-14 20:34:57 +0530

    modified:   Server/database/db.js 	modified:   Server/index.js 	modified:   Server/package-lock.json 	modified:   Server/package.json

--- Server/database/db.js
+++ Server/database/db.js
@@ -1,2 +1,13 @@
+import mongoose from "mongoose";
 
+const DBconnection=async ()=>{
+    const MONGO_URL='mongodb+srv://FiddiCode:fileSharing@file-sharing-cluster.izew0z9.mongodb.net/?retryWrites=true&w=majority';
+   try {
+        await mongoose.connect(MONGO_URL,{useNewUrlParser:true});
+        console.log('Database connected Successfully');
+   } catch (error) {
+     console.log('Error while connecting to database',error.message);
+   }
+}
 
+export default DBconnection;
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import router from './Routes/route.js';
 import cors from 'cors'
+import DBConnection from './database/db.js'
 
 const app=express();
 const PORT=process.env.PORT || 8000
@@ -9,6 +10,8 @@ const PORT=process.env.PORT || 8000
 app.use(cors());
 app.use('/',router);
 
+
+DBConnection();
 
 app.listen(PORT,()=>{
     console.log(`Server is running at port ${PORT}`); 
