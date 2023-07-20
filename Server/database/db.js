985430bd4cd8adc3f4e9447692aed501a4d2a248
fiddicode <coderfardeen@gmail.com>
2023-07-20 08:14:13 +0530

    new file:   Server/.env 	modified:   Server/database/db.js 	modified:   Server/index.js 	modified:   Server/package-lock.json 	modified:   Server/package.json 	new file:   Server/uploads/e9b10e9cb17575dc95feee754afe3568

--- Server/database/db.js
+++ Server/database/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const DBconnection=async ()=>{
-    const MONGO_URL='mongodb+srv://FiddiCode:fileSharing@file-sharing-cluster.izew0z9.mongodb.net/?retryWrites=true&w=majority';
+    
    try {
-        await mongoose.connect(MONGO_URL,{useNewUrlParser:true});
+        await mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true});
         console.log('Database connected Successfully');
    } catch (error) {
      console.log('Error while connecting to database',error.message);
--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import router from './Routes/route.js';
 import cors from 'cors'
+import dotenv from 'dotenv'
 import DBConnection from './database/db.js'
 
 const app=express();
+dotenv.config();
 const PORT=process.env.PORT || 8000
 
 
